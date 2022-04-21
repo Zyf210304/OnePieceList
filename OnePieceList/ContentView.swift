0ea10dda879f83dd20b393628cbc3f9ef336b3dc
张亚飞 <672233580@qq.com>
2022-04-21 09:26:37 +0800

    Initial Commit

---
+++ OnePieceList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnePieceList
+//
+//  Created by 张亚飞 on 2022/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnePieceList/OnePieceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePieceListApp.swift
+//  OnePieceList
+//
+//  Created by 张亚飞 on 2022/4/21.
+//
+
+import SwiftUI
+
+@main
+struct OnePieceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
